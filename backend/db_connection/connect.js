232691d73ab7038e9a17e05a70d4cee0ea0c8f66
vikram <vikramp873@gmail.com>
2023-01-19 18:57:05 +0530

    changes

--- backend/db_connection/connect.js
+++ backend/db_connection/connect.js
@@ -1,20 +1,30 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/ecomm');
-const DB = 'mongodb+srv://rajip873:9224265644@cluster0.rrgpysf.mongodb.net/ecomm'
+const DB = process.env.DB
 
 
+mongoose.set('strictQuery', true);
+try {
+   console.log('hi')
 
-mongoose.connect(DB, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-   autoIndex: false, // Don't build indexes
-   family: 4 // Use IPv4, skip trying IPv6
-}).then(() => {
-   console.log('connection successfully')
-})
-   .catch((err) => {
-      console.log('no connection', err)
+   mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false, // Don't build indexes
+      family: 4 // Use IPv4, skip trying IPv6
+   }).then(() => {
+      console.log('connection successfully')
+      console.log('hi')
+
    })
+      .catch((err) => {
+         console.log('no connection', err)
+      })
+
+}
+catch (err) {
+   console.log(err)
+}
 
 
 
